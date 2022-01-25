8e72bf4d574a9ecc20bbb9103fef48ce38f8ee49
Venkatesh0625 <venkatesan25@outlook.com>
2022-01-26 03:34:25 +0530

    add base58 conversion in solwallet

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 
 import { useEffect, useState } from 'react';
-import Web3 from 'web3';
 import EthereumWallet from './wallets/etherumWallet';
-import {ethers }from "ethers"
 import SolanaWallet from './wallets/solanaWallet';
 
 const App = () => {
 
-  // const wallet = new EthereumWallet(
-  //   "lend custom palace wrestle flee gravity toward fitness wasp economy task smooth", 
-  //   'https://mainnet.infura.io/v3/f48a9c5974b3421d867defd988e4385a')
+  const ethwallet = new EthereumWallet(
+    "lend custom palace wrestle flee gravity toward fitness wasp economy task smooth", 
+    'https://mainnet.infura.io/v3/f48a9c5974b3421d867defd988e4385a')
 
-  const wallet = new SolanaWallet(
+  const solwallet = new SolanaWallet(
     "lend custom palace wrestle flee gravity toward fitness wasp economy task smooth",
     "https://api.mainnet-beta.solana.com"
+  )
+
+  const bscwallet = new EthereumWallet(
+    "lend custom palace wrestle flee gravity toward fitness wasp economy task smooth", 
+    'https://bsc-dataseed.binance.org/'
   )
   
 
   let [bal, setbal] = useState({})
   useEffect(async () => {
-   
-      var x = await wallet.signTransaction("0x61c0002a0A85C8f2f2f184195c1E016bc2D845E0", "1").then(console.log, console.log)
-
-    
+      solwallet.signTransaction("HaYnRYjtaHvxQgmvUQhUk6ic2oVJbPyMVYgMMt4L7z5K", "1").then(console.log, console.log)
+      ethwallet.signTransaction("0x61c0002a0A85C8f2f2f184195c1E016bc2D845E0", "1").then(console.log, console.log)
+      bscwallet.signTransaction("0x61c0002a0A85C8f2f2f184195c1E016bc2D845E0", "1").then(console.log, console.log)
   }, [])
   
   return (
